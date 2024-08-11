3433f69bee054ba3a8abb858dc41ad92cdf7ba54
ksawerykowalczuk <ksawery.kowalczuk@gmail.com>
2024-08-11 20:47:03 +0200

    end

--- mostek.cpp
+++ mostek.cpp
@@ -1,63 +1,56 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int iloscludzi;
-    cin >> iloscludzi;
-    int ludzie[iloscludzi];
-
-    for (int i = 0; i < iloscludzi; i++)
-    {
-        cin >> ludzie[i];
-    }
-
-    for (int i = 0; i < iloscludzi - 1; i++)
-    {
-        int minimum = i;
-        for (int j = i + 1; j < iloscludzi; j++)
-        {
-            if (ludzie[j] < ludzie[minimum])
-            {
-                minimum = j;
+void sortujOdNajszybszego(int czasy[], int iloscludzi) {
+    bool czyzamienieni = true;
+    while (czyzamienieni){
+        czyzamienieni = false;
+        for (int i = 0; i < iloscludzi - 1; i++) {
+            if (czasy[i] > czasy[i + 1]) {
+                int temporarnaZmienna = czasy[i];
+                czasy[i] = czasy[i + 1];
+                czasy[i + 1] = temporarnaZmienna;
+                czyzamienieni = true;
             }
         }
+        iloscludzi--;
+    }
+}
 
-        int temp = ludzie[minimum];
-        ludzie[minimum] = ludzie[i];
-        ludzie[i] = temp;
+int przejscieMostu(int czasy[], int iloscludzi) {
+    if (iloscludzi == 1) return czasy[0];
+    if (iloscludzi == 2) return czasy[1];
+
+    int minimalnyCzas = 0;
+
+    while (iloscludzi > 3) {
+        int czas_szybki = czasy[1] + czasy[0] + czasy[iloscludzi-1] + czasy[1];
+        int czas_wolny = czasy[iloscludzi-1] + czasy[iloscludzi-2] + 2 * czasy[0];
+        minimalnyCzas += min(czas_szybki, czas_wolny);
+        iloscludzi -= 2;
     }
 
-    int czasomierz = 0;
-
-    while (iloscludzi > 3)
-    {
-        int czas1 = 2 * ludzie[0] + ludzie[iloscludzi - 1] + ludzie[iloscludzi - 2];
-        int czas2 = 2 * ludzie[1] + ludzie[0] + ludzie[iloscludzi - 1];
-
-        if (czas1 < czas2)
-        {
-            czasomierz = czasomierz + czas1;
-        }
-        else
-        {
-            czasomierz = czasomierz + czas2;
-        }
-        iloscludzi = iloscludzi - 2;
+    if (iloscludzi == 3) {
+        minimalnyCzas += czasy[2] + czasy[0] + czasy[1];
+    } else if (iloscludzi == 2) {
+        minimalnyCzas += czasy[1];
     }
 
-    if (iloscludzi == 3)
-    {
-        czasomierz = czasomierz + ludzie[0] + ludzie[1] + ludzie[2];
-    }
-    if (iloscludzi == 2)
-    {
-        czasomierz = czasomierz + ludzie[1];
-    }
-    if (iloscludzi == 1)
-    {
-        czasomierz = czasomierz + ludzie[0];
+    return minimalnyCzas;
+}
+
+int main() {
+    int iloscludzi;
+    cin >> iloscludzi;
+
+    int czasy[iloscludzi];
+    for (int i = 0; i < iloscludzi; i++) {
+        cin >> czasy[i];
     }
 
-    cout << czasomierz;
+    sortujOdNajszybszego(czasy, iloscludzi);
+
+    int minimalnyCzas = przejscieMostu(czasy, iloscludzi);
+    
+    cout << minimalnyCzas << endl;
 }
